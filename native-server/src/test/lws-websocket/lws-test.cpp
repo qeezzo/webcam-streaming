3eb9e6e9606a271b18befadaabae7d575df2a841
egbor <egorminsk2001@gmail.com>
2025-04-25 11:25:31 +0100

    Added incomming message concatenation

--- native-server/src/test/lws-websocket/lws-test.cpp
+++ native-server/src/test/lws-websocket/lws-test.cpp
@@ -1,28 +1,41 @@
 #include <libwebsockets.h>
 #include <signal.h>
+
 #include <iostream>
+#include <string>
 
 static int interrupted = 0;
+
+static struct lws_protocols protocols[] = {
+    {"ws", callback_echo, 0, 0},
+    {NULL, NULL, 0, 0}
+};
 
 void sigint_handler(int sig) {
 	interrupted = 1;
 }
 
+void handle_incomming_message(const std::string& message) {
+    
+}
+
 int callback_echo(struct lws* wsi, enum lws_callback_reasons reason, void* user, void* in, size_t len) {
+    static std::string message;
+
     switch (reason) {
         case LWS_CALLBACK_RECEIVE:
-            lwsl_user("Received: (%d)%s\n", lws_is_final_fragment(wsi), (const char *)in);
+            message += std::string(static_cast<const char*>(in), len);
+            if (lws_is_final_fragment(wsi) != 0) {
+                lwsl_user("Received: (%d-%llu)%s\n", lws_is_final_fragment(wsi), message.size(), message.data());
+                handle_incomming_message(message);
+                message.resize(0);
+            }
             break;
         default:
             break;
     }
     return 0;
 }
-
-static struct lws_protocols protocols[] = {
-    {"ws", callback_echo, 0, 0},
-    {NULL, NULL, 0, 0}
-};
 
 int main(int argc, char* argv[]) {
     lws_context_creation_info info;
@@ -38,7 +51,6 @@ int main(int argc, char* argv[]) {
 
 	memset(&info, 0, sizeof(info));
 	info.port = 3000;
-	// info.mounts = &mount;
 	info.protocols = protocols;
 	info.vhost_name = "localhost";
 
